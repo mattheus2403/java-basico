f6e4d8ddacf2b6120079321796e96fe6082906f9
Mattheus2403 <matheusmoraidois@gmail.com>
2023-09-05 17:56:47 -0300

    feat: add BoletimEstudantil.java

---
+++ java-anatomia-de-classes/src/com/classes/BoletimEstudantil.java
@@ -0,0 +1,16 @@
+package com.classes;
+
+public class BoletimEstudantil {
+
+	public static void main(String[] args) {
+		int mediaFinal = 8;
+		if (mediaFinal < 6)
+			System.out.println("REPROVADO");
+		else if (mediaFinal == 6)
+			System.out.println("PROVA MINERVA");
+		else
+			System.out.println("APROVADO");
+
+	}
+
+}
