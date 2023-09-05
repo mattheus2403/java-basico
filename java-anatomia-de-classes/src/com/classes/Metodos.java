10423a4f14d2f40d63c6a1510c58f16700a77d55
Mattheus2403 <matheusmoraidois@gmail.com>
2023-09-05 17:21:27 -0300

    feat: add Variaveis.java

---
+++ java-anatomia-de-classes/src/com/classes/Metodos.java
@@ -0,0 +1,25 @@
+package com.classes;
+
+public class Metodos {
+
+	public static void main(String[] args) {
+		
+		//declarando valores do método
+		String primeiroNome = "Matheus";
+		String segundoNome = "Morais";
+		
+		//variável que irá chamar o método
+		String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+		
+		System.out.print(nomeCompleto);
+		
+	}
+	
+	//declaração do método
+	public static String nomeCompleto(String primeiroNome, String segundoNome) {
+		
+		//o que será retornado e a concatenação das duas variáveis
+		return primeiroNome.concat(" ").concat(segundoNome);
+	}
+
+}
---
+++ java-anatomia-de-classes/src/com/classes/Variaveis.java
@@ -0,0 +1,21 @@
+package com.classes;
+public class Variaveis {
+
+	public static void main(String[] args) {
+
+		// declaração de variáveis
+
+		// texto
+		String meuNome = "Matheus Morais";
+
+		// inteiro
+		int anoFabricacao = 2022;
+
+		// lógico
+		boolean verdadeira = true;
+
+		// valor modificado
+		anoFabricacao = 2018;
+	}
+
+}
