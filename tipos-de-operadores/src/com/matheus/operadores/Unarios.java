538b3ab9567483b2af91d65f6170a729a97db725
Mattheus2403 <matheusmoraidois@gmail.com>
2023-09-06 20:48:01 -0300

    feat: add class Unarios.java

---
+++ tipos-de-operadores/src/com/matheus/operadores/Unarios.java
@@ -0,0 +1,25 @@
+package com.matheus.operadores;
+
+public class Unarios {
+	
+	public static void main (String args[]) {
+		
+		//valores unarios definem se um numero
+		//poderá ser negativo ou positivo
+		
+		int num = 5;
+		
+		//torna a variável negativa
+		num = - num;
+		
+		System.out.println(num);
+		
+		//torna a variável positiva
+		num = num * -1;
+		
+		System.out.println(num);
+		
+		
+	}
+
+}
