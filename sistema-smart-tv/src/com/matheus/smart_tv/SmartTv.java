fceb085a0f09299ca2a081012995eda0f707cf5d
Mattheus2403 <matheusmoraidois@gmail.com>
2023-09-08 20:54:56 -0300

    docs: add comments in class SmartTv.java

--- sistema-smart-tv/src/com/matheus/smart_tv/SmartTv.java
+++ sistema-smart-tv/src/com/matheus/smart_tv/SmartTv.java
@@ -5,35 +5,42 @@ public class SmartTv {
 	boolean ligada = false;
 	int canal = 1;
 	int volume = 25;
-		
+	
+	//liga a televisão
 	public void ligar() {
 		ligada = true;
 	}
 	
+	//desliga a televisão
 	public void desligar() {
 		ligada = false;
 	}
 	
+	//aumenta volume da televisão
 	public void aumentarVolume() {
 		volume++;
 		System.out.println("Aumentando o volume para " + volume + "\n");
 	}
 	
+	//diminui volume da televisão
 	public void abaixarVolume() {
 		volume--;
 		System.out.println("Diminuindo o volume para " + volume + "\n");
 	}
 	
+	//muda para um canal selecionado
 	public void mudarCanal(int novoCanal) {
 		canal = novoCanal;
 		System.out.println("Novo Canal: " + canal + "\n");
 	}
 	
+	//sobe 1 canal a cima ou avança a frente
 	public void subirCanal() {
 		canal++;
 		System.out.println("Novo Canal: " + canal + "\n");
 	}
 	
+	//desce 1 canal abaixo ou volta atrás
 	public void diminuirCanal() {
 		canal--;
 		System.out.println("Novo Canal: " + canal + "\n");
