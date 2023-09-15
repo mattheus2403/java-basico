ac9d4bf1122d3ec9a0ae90fe60b4c5477826e8cf
Mattheus2403 <matheusmoraidois@gmail.com>
2023-09-15 19:05:02 -0300

    feat: add class SwitchCaseFuncional.java

---
+++ estruturas-condicionais/src/com/matheus/condicionais/SwitchCaseFuncional.java
@@ -0,0 +1,38 @@
+package com.matheus.condicionais;
+
+public class SwitchCaseFuncional {
+
+	public static void main(String[] args) {
+		// exemplo de switch case funcional, onde
+		// a falta do case complementa o exemplo de
+		// planos de celular basic, midia e turbo
+		//para que não se repitam impressões
+		
+		String plano = "T";
+		
+		switch(plano) {
+		
+		//5GB + Whats e Insta + 100 min
+		case "T":{
+			System.out.println("5GB Youtube");
+		}
+		
+		//whats e Insta + 100 min
+		case "M":{
+			System.out.println("Whatsapp e Instagram Grátis");
+		}
+		
+		//100 min
+		case "B":{
+			System.out.println("100 minutos de ligação");
+			break; //break para separar do default
+		}
+		
+		default: {
+			System.out.println("Indefinido");
+		}
+		}
+
+	}
+
+}
