7cf767ad066df31c6a6d191266a8a34f319bdb8e
Mattheus2403 <matheusmoraidois@gmail.com>
2023-09-15 17:00:58 -0300

    feat: add class CondicionalSimples.java

---
+++ estruturas-condicionais/src/com/matheus/condicionais/CondicionalSimples.java
@@ -0,0 +1,23 @@
+package com.matheus.condicionais;
+
+public class CondicionalSimples {
+
+	public static void main(String[] args) {
+		
+		//uma condicional simples toma uma decisão onde apenas retornará
+		//algo se a resposta for verdadeira
+		
+		double saldo = 24.0;
+		double valorSolicitado = 14.0;
+		
+		//verifica se há saldo suficiente para saque
+		if (saldo > valorSolicitado) {
+			saldo = saldo - valorSolicitado;
+		}
+		
+		System.out.println(saldo);
+				
+
+	}
+
+}
