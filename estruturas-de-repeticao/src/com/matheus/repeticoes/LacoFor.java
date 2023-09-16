afb3071bf30ebee00da240feb415b8fcd6f73b51
Mattheus2403 <matheusmoraidois@gmail.com>
2023-09-15 21:55:57 -0300

    feat: add class LacoFor.java

---
+++ estruturas-de-repeticao/src/com/matheus/repeticoes/LacoFor.java
@@ -0,0 +1,22 @@
+package com.matheus.repeticoes;
+
+public class LacoFor {
+
+	public static void main(String[] args) {
+		// O laço de repetição for, é capaz
+		//de repetir um bloco de código em
+		//um determinado número de vezes
+		//necessarias adicionando o número de inicio,
+		//a condição paro o fim do laço de repetição
+		//e em quanto ele irá incrementar ou decrementar
+		
+		//conta do 1 até 10
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("Número: " + i);
+		}
+		
+		System.out.println("Contagem terminada");
+
+	}
+
+}
