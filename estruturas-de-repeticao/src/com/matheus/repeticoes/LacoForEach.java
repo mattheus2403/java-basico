9abbae2430ed794da0cfe4ccee23e43ffe1fbe37
Mattheus2403 <matheusmoraidois@gmail.com>
2023-09-15 22:13:48 -0300

    feat: add class LacoForEach

---
+++ estruturas-de-repeticao/src/com/matheus/repeticoes/LacoForEach.java
@@ -0,0 +1,21 @@
+package com.matheus.repeticoes;
+
+public class LacoForEach {
+
+	public static void main(String[] args) {
+		// O laço for each (para cada) é geralmente voltado
+		//e recomendado para estruturas de repetição
+		//que envolvam uso de arrays ou coleções
+		//ele também funciona de forma abreviada
+		//em relação ao for normal com arrays
+		
+		String alunos[] = {"Matheus", "Alessandro", "João", "Maria"};
+		
+		//aluno irá repetir até atravessar cada aluno dentro do array
+		for (String aluno : alunos) {
+			System.out.println(aluno);
+		}
+
+	}
+
+}
