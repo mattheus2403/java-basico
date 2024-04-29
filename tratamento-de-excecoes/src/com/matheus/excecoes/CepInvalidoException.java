0d082833b4a76519d7062fcc28af9c82f4da4926
Mattheus2403 <matheusmoraidois@gmail.com>
2024-04-29 19:34:25 -0300

    feat: add project java-debugging

--- tratamento-de-excecoes/src/com/matheus/excecoes/CepInvalidoException.java
+++ tratamento-de-excecoes/src/com/matheus/excecoes/CepInvalidoException.java
@@ -1,4 +1,4 @@
 package com.matheus.excecoes;
 
-//classe de excessao customizada propriamente pra aplicação FomatadorCep.java
+//classe de excessao customizada propriamente pra aplicação FormatadorCep.java
 public class CepInvalidoException extends Exception {}
