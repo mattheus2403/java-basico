b3ec3a71928add619f15ebac2114ac2f116bcd22
Mattheus2403 <matheusmoraidois@gmail.com>
2024-05-04 22:05:08 -0300

    feat: selecionando candidatos

--- sistema-validacao-processo-seletivo/src/com/matheus/selecao/ProcessoSeletivo.java
+++ sistema-validacao-processo-seletivo/src/com/matheus/selecao/ProcessoSeletivo.java
@@ -1,12 +1,39 @@
 package com.matheus.selecao;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		analisarCandidato(1900.0);
-		analisarCandidato(2000.0);
-		analisarCandidato(2200.0);
+//		analisarCandidato(1900.0);
+//		analisarCandidato(2000.0);
+//		analisarCandidato(2200.0);
+		
+		selecaoCandidatos();
+	}
+	
+	static void selecaoCandidatos() {
+		String candidatos[] = {"FELIPE","MARCIA","PAULO","ALGUSTO","FABRICIO","MIRELA","DANIELA","JORGE"};
+		
+		int candidatosSelecionados = 0;
+		int candidatoAtual = 0;
+		double salarioBase = 2000.0;
+		
+		while(candidatosSelecionados <= 5 && candidatoAtual < candidatos.length) {
+			String candidato = candidatos[candidatoAtual];
+			double salarioPretendido = valorPretendido();
+			System.out.println("O candidato: " + candidato + " solicitou este valor de salário: " + salarioPretendido);
+			if (salarioBase >= salarioPretendido) {
+				System.out.print("O candidato: " + candidato + " foi selecionado para a vaga\n");
+				candidatosSelecionados++;
+			}
+			candidatoAtual ++;
+		}
+	}
+	
+	static double valorPretendido() {
+		return ThreadLocalRandom.current().nextDouble(1800, 2200);
 	}
 	
 	static void analisarCandidato (double salarioPretendido) {
@@ -21,5 +48,6 @@ public class ProcessoSeletivo {
 			System.out.println("Aguardando demais candidatos");
 		}
 	}
+
 
 }
