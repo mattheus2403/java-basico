d57bc0e827d0df88f71bc9519fb3c0d99695b048
Mattheus2403 <matheusmoraidois@gmail.com>
2024-05-04 22:57:01 -0300

    feat: ligando para candidatos

--- sistema-validacao-processo-seletivo/src/com/matheus/selecao/ProcessoSeletivo.java
+++ sistema-validacao-processo-seletivo/src/com/matheus/selecao/ProcessoSeletivo.java
@@ -1,18 +1,50 @@
 package com.matheus.selecao;
 
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.Random;
 
 public class ProcessoSeletivo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 //		analisarCandidato(1900.0);
 //		analisarCandidato(2000.0);
 //		analisarCandidato(2200.0);
 		
 //		selecaoCandidatos();
 		
-		imprimirSelecionados();
+//		imprimirSelecionados();
+		
+		String candidatos[] = {"FELIPE","MARCIA","PAULO","ALGUSTO","FABRICIO"};
+		for (String candidato : candidatos) {
+			entrandoEmContato(candidato);
+		}
+	}
+	
+	static void entrandoEmContato(String candidato) {
+		int tentativasRealizadas = 1;
+		boolean continuarTentando = true;
+		boolean atendeu = false;
+		do {
+			atendeu = atender();
+			continuarTentando = !atendeu;
+			if (continuarTentando) {
+				tentativasRealizadas++;
+			}
+			else {
+				System.out.println("CONTATO REALIZADO COM SUCESSO");
+			}
+		}while(continuarTentando && tentativasRealizadas < 3);
+		
+		if (atendeu) {
+			System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + "º TENTATIVA");
+		}
+		else {
+			System.out.println("NAO CONSEGUIMOS CONTATO COM " + candidato + " NUMERO MAXIMO DE TENTATIVAS " + tentativasRealizadas + " REALIZADAS");
+		}
+	}
+	
+	static boolean atender() {
+		return new Random().nextInt(3)==1;
 	}
 	
 	static void imprimirSelecionados() {
