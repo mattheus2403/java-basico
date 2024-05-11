49a2d0f4ac3c69589f33f87ee7d4720676a19e52
Mattheus2403 <matheusmoraidois@gmail.com>
2024-05-10 21:09:26 -0300

    update: class SistemaIbge.java and enum EstadoBrasileiro.java

--- programacao-orientada-a-objetos/src/poo/enumeradores/EstadoBrasileiro.java
+++ programacao-orientada-a-objetos/src/poo/enumeradores/EstadoBrasileiro.java
@@ -2,23 +2,28 @@ package poo.enumeradores;
 
 //Criando o enum EstadoBrasileiro para ser usado em toda a aplicação.
 public enum EstadoBrasileiro {
-	SAO_PAULO ("SP", "São Paulo"),
-	RIO_DE_JANEIRO ("RJ", "Rio de Janeiro"),
-	MINAS_GERAIS ("MG", "Minas Gerais"),
-	RIO_GRANDE_DO_SUL ("RS", "Rio Grande do Sul");
+	SAO_PAULO ("SP", "São Paulo", 11),
+	RIO_DE_JANEIRO ("RJ", "Rio de Janeiro", 12),
+	MINAS_GERAIS ("MG", "Minas Gerais", 13),
+	RIO_GRANDE_DO_SUL ("RS", "Rio Grande do Sul", 14);
 	
 	private String nome;
 	private String sigla;
+	private int ibge;
 	
-	private EstadoBrasileiro(String sigla, String nome) {
+	private EstadoBrasileiro(String sigla, String nome, int ibge) {
 		this.sigla = sigla;
 		this.nome = nome;
+		this.ibge = ibge;
 	}
 	public String getSigla() {
 		return sigla;
 	}
 	public String getNome() {
 		return nome;
+	}
+	public int getIbge() {
+		return ibge;
 	}
 	public String getNomeMaiusculo() {
 		return nome.toUpperCase();
--- programacao-orientada-a-objetos/src/poo/enumeradores/SistemaIbge.java
+++ programacao-orientada-a-objetos/src/poo/enumeradores/SistemaIbge.java
@@ -3,17 +3,22 @@ package poo.enumeradores;
 public class SistemaIbge {
 
 	public static void main(String[] args) {
-		// Irei chamar os atributos do enum EstadoBrasileiro até que todos
+		// Irei chamar os estados do enum EstadoBrasileiro até que todos
 		// sejam exibidos com nome, sigla e nome maiusculo
+		System.out.println("TODOS OS ESTADOS DO ENUM: \n");
 		for (EstadoBrasileiro e : EstadoBrasileiro.values()) {
-			System.out.println(e.getNome() + " - " + e.getSigla() + "\nEm Maiusculo: " + e.getNomeMaiusculo());
+			System.out.println(e.getNome() + " - " + e.getSigla() + " - " + e.getIbge() + "\nEm Maiusculo: " + e.getNomeMaiusculo());
 		}
 		
 		// chama e associa especificamente ao estado de são paulo
 		EstadoBrasileiro sp = EstadoBrasileiro.SAO_PAULO;
 		System.out.println("\nNome do estado em particular: " + sp.getNome() + "\nSigla do estado em particular: " + sp.getSigla() 
-		+ "\nNome Maiusculo do estado em particular: " + sp.getNomeMaiusculo());
+		+ "\nNuméro IBGE do estado em particular: " + sp.getIbge() + "\nNome Maiusculo do estado em particular: " + sp.getNomeMaiusculo());
 		
+		// outro estado
+		EstadoBrasileiro mg = EstadoBrasileiro.MINAS_GERAIS;
+		System.out.println("\nNome do estado em particular: " + mg.getNome() + "\nSigla do estado em particular: " + mg.getSigla() 
+		+ "\nNuméro IBGE do estado em particular: " + mg.getIbge() + "\nNome Maiusculo do estado em particular: " + mg.getNomeMaiusculo());
 	}
 
 }
