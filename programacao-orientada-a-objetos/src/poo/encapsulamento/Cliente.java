5595a8e8039cf07c328a5a439e5972afe50aecea
Mattheus2403 <matheusmoraidois@gmail.com>
2024-05-17 21:08:43 -0300

    refactor: rename class Hamburgueria.java and Client.java

--- programacao-orientada-a-objetos/src/poo/encapsulamento/Cliente.java
+++ programacao-orientada-a-objetos/src/poo/encapsulamento/Cliente.java
@@ -1,15 +1,23 @@
 package poo.encapsulamento;
 
 public class Cliente {
-
-	public static void main(String[] args) {
-
-		Hamburgueria hamburgueria = new Hamburgueria();
-		
-		//chama os métodos privados que estão dentro do método publico de pedido
-		hamburgueria.pedido();
-		
-		hamburgueria.pagarConta();
+	
+	//exemplo de encapsulamento colocando métodos private dentro de um método public
+	public void pedido() {
+		System.out.println("Anotando pedido do cliente");
+		preparo();
+		pegarPedido();
 	}
-
+	
+	private void preparo() {
+		System.out.println("Preparando pedido");
+	}
+	
+	private void pegarPedido() {
+		System.out.println("Pegar pedido no balcão e levar ao cliente");
+	}
+	
+	public void pagarConta() {
+		System.out.println("Pagando a conta");
+	}
 }
--- programacao-orientada-a-objetos/src/poo/encapsulamento/Hamburgueria.java
+++ programacao-orientada-a-objetos/src/poo/encapsulamento/Hamburgueria.java
@@ -1,23 +1,15 @@
 package poo.encapsulamento;
 
 public class Hamburgueria {
-	
-	//exemplo de encapsulamento colocando métodos private dentro de um método public
-	public void pedido() {
-		System.out.println("Anotando pedido do cliente");
-		preparo();
-		pegarPedido();
+
+	public static void main(String[] args) {
+
+		Cliente cliente = new Cliente();
+		
+		//chama os métodos privados que estão dentro do método publico de pedido
+		cliente.pedido();
+		
+		cliente.pagarConta();
 	}
-	
-	private void preparo() {
-		System.out.println("Preparando pedido");
-	}
-	
-	private void pegarPedido() {
-		System.out.println("Pegar pedido no balcão e levar ao cliente");
-	}
-	
-	public void pagarConta() {
-		System.out.println("Pagando a conta");
-	}
+
 }
