831f93623f0c4f9e064a3a325de177974d1824eb
Mattheus2403 <matheusmoraidois@gmail.com>
2024-05-20 19:16:48 -0300

    feat: add abstract class ServicoPreparaSuco to package poo.abstracao/

---
+++ programacao-orientada-a-objetos/src/poo/abstracao/ServicoPreparoSucos.java
@@ -0,0 +1,9 @@
+package poo.abstracao;
+
+public abstract class ServicoPreparoSucos {
+	
+	// classes abstratas permitem que cada classe faça a declaração de sua própria
+	
+	public abstract void preparaSuco();
+	public abstract void entragaSuco();
+}
