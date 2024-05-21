65b957e5f8ff8ef6db0a640f71162820e90128c1
Mattheus2403 <matheusmoraidois@gmail.com>
2024-05-20 21:36:08 -0300

    feat: add class Pastelaria.java to package poo.polimorfismo.pastelaria/

---
+++ programacao-orientada-a-objetos/src/poo/polimorfismo/pastelaria/Pastelaria.java
@@ -0,0 +1,42 @@
+package poo.polimorfismo.pastelaria;
+
+import java.util.Scanner;
+
+import poo.polimorfismo.pastelaria.cozinha.PastelCarne;
+import poo.polimorfismo.pastelaria.cozinha.PastelFrango;
+import poo.polimorfismo.pastelaria.cozinha.PastelQueijo;
+import poo.polimorfismo.pastelaria.cozinha.ServicoPastelaria;
+
+public class Pastelaria {
+
+	public static void main(String[] args) {
+		// TODO Exemplo de polimorfismo importando classes
+		// filhas de classes abstradas declaradas e não declaradas
+		
+		//leitor
+		Scanner leitor = new Scanner(System.in);
+		
+		//construtores
+		ServicoPastelaria pdp = null; //não se sabe qual será o pedido escolhido
+		
+		//entrada do pedido de pastel
+		System.out.println("Qual Pastel deseja pedir?");
+		String PedidoPastel = leitor.next();
+		
+		//algoritmo de decisão do pastel
+		if (PedidoPastel.equals("carne")) {
+			pdp = new PastelCarne();
+		}
+		else if (PedidoPastel.equals("queijo")) {
+			pdp = new PastelQueijo();
+		}
+		else if (PedidoPastel.equals("frango")) {
+			pdp = new PastelFrango();
+		}
+		
+		pdp.anotarPedido();
+		pdp.entragaPastel();
+		
+	}
+
+}
