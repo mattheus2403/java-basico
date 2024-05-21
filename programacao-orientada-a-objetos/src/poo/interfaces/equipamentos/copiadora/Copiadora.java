1119773de3b0d6d4410dafb384258fa16b4bff5e
Mattheus2403 <matheusmoraidois@gmail.com>
2024-05-21 00:42:39 -0300

    feat: add interface Copiadora.java and class Xerox.java to package poo.interfaces.equipamentos.copiadora/

---
+++ programacao-orientada-a-objetos/src/poo/interfaces/equipamentos/copiadora/Copiadora.java
@@ -0,0 +1,6 @@
+package poo.interfaces.equipamentos.copiadora;
+
+public interface Copiadora { //interfaces funcionam como classes abstratas que permitem heranca multipla
+	
+	public void copiar();
+}
---
+++ programacao-orientada-a-objetos/src/poo/interfaces/equipamentos/copiadora/Xerox.java
@@ -0,0 +1,10 @@
+package poo.interfaces.equipamentos.copiadora;
+
+public class Xerox implements Copiadora{ //implementação da interface
+
+	@Override
+	public void copiar() {
+		System.out.println("Copiando");
+	}
+
+}
