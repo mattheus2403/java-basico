0b5b1cb8b00d45eeceeec91fa8ec976ad706a17c
Mattheus2403 <matheusmoraidois@gmail.com>
2024-05-21 00:57:45 -0300

    feat: add class LaserJet.java, DeskJet.java and add interface Impressora.java to package poo.interfaces.equipamentos.impressora/

---
+++ programacao-orientada-a-objetos/src/poo/interfaces/equipamentos/impressora/DeskJet.java
@@ -0,0 +1,10 @@
+package poo.interfaces.equipamentos.impressora;
+
+public class DeskJet implements Impressora {
+
+	@Override
+	public void imprimir() {
+		System.out.println("Imprimindo");
+	}
+
+}
---
+++ programacao-orientada-a-objetos/src/poo/interfaces/equipamentos/impressora/LaserJet.java
@@ -0,0 +1,10 @@
+package poo.interfaces.equipamentos.impressora;
+
+public class LaserJet implements Impressora{
+
+	@Override
+	public void imprimir() {
+		System.out.println("Imprimindo");
+	}
+
+}
