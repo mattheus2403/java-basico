bcc2efedced4b0cf3cab72f1de081790e7a73497
Mattheus2403 <matheusmoraidois@gmail.com>
2024-05-28 01:15:38 -0300

    feat: add class Tarefa.java to package collections/list/OperacoesBasicas/

---
+++ java-collections/src/collections/list/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,39 @@
+package collections.list.OperacoesBasicas;
+
+public class Tarefa {
+
+	//atributo
+	private String descricao;
+	
+	
+	public Tarefa(String descricao) {
+		super();
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+
+	private void adicionarTarefa() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void removerTarefa() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void obterNumeroTotalTarefas() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void obterDescricoesTarefas() {
+		// TODO Auto-generated method stub
+
+	}
+}
