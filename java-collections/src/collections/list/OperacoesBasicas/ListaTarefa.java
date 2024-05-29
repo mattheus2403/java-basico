444987c7ac60d8e2b5c3626856fd3a6cfe55a19a
Mattheus2403 <matheusmoraidois@gmail.com>
2024-05-28 21:29:37 -0300

    docs: add comments to class Tarefa.java and ListaTarefa.java

--- java-collections/src/collections/list/OperacoesBasicas/ListaTarefa.java
+++ java-collections/src/collections/list/OperacoesBasicas/ListaTarefa.java
@@ -6,47 +6,61 @@ import java.util.List;
 public class ListaTarefa {
 
 	//atributo
-	private List <Tarefa> tarefaList;
+	private List <Tarefa> tarefaList; //cria a lista tarefaList colocando a classe Tarefa como elemento generico
 
+	//construtor que cria uma ArrayList vazia para manipulação de elementos da list
 	public ListaTarefa() {
 		super();
 		this.tarefaList = new ArrayList<>();
 	}
 	
+	//método que adiciona tarefa a List 
 	private void adicionarTarefa(String descricao) {
 		tarefaList.add(new Tarefa(descricao));
 	}
 	
+	//método que remove tarefa da List
 	private void removerTarefa(String descricao) {
-		List <Tarefa> tarefasParaRemover = new ArrayList<>();
+		List <Tarefa> tarefasParaRemover = new ArrayList<>(); //cria uma nova List com tarefas para remover
+		//cria laço for que percorre a List tarefaList e adiciona o item de descricao a List tarefasParaRemover
 		for(Tarefa t : tarefaList) {
 			if(t.getDescricao().equalsIgnoreCase(descricao)) {
 				tarefasParaRemover.add(t);
 			}
 		}
-		tarefaList.removeAll(tarefasParaRemover);
+		tarefaList.removeAll(tarefasParaRemover); //remove todos os itens na List de tarefas para remover
 	}
 	
+	//obtem o tamanho da List
 	private int obterNumeroTotalTarefas() {
 		return tarefaList.size();
 	}
 	
+	//Imprime descrição das tarefas da List
 	private void obterDescricoesTarefas() {
 		System.out.println(tarefaList);
 	}
 	
+	//método principal
 	public static void main(String[] args) {
+		//Cria objeto simples para chamada de métodos declarados anteriormente
 		ListaTarefa listaTarefa = new ListaTarefa();
 		
+		//adiciona tarefas
 		listaTarefa.adicionarTarefa("tarefa 1");
 		listaTarefa.adicionarTarefa("tarefa 1");
 		listaTarefa.adicionarTarefa("tarefa 2");
+		
+		//impressao total de tarefas
 		System.out.println("O numero total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
 		
-		listaTarefa.removerTarefa("tarefa 1");
+		//remove tarefa
+		listaTarefa.removerTarefa("tarefa 1"); //ao pedir para remover a tarefa 1, caso esteja duplicada todas com esse nome são removidas
 		
+		//impressao total de tarefas
 		System.out.println("O numero total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
 		
+		//imprimi descrição de tarefa
 		listaTarefa.obterDescricoesTarefas();
 	}
 	
--- java-collections/src/collections/list/OperacoesBasicas/Tarefa.java
+++ java-collections/src/collections/list/OperacoesBasicas/Tarefa.java
@@ -5,7 +5,7 @@ public class Tarefa {
 	//atributo
 	private String descricao;
 	
-	
+	//método construtor
 	public Tarefa(String descricao) {
 		super();
 		this.descricao = descricao;
@@ -15,6 +15,7 @@ public class Tarefa {
 		return descricao;
 	}
 
+	//método toString que constrói uma estutura para decricao
 	@Override
 	public String toString() {
 		//return "Tarefa [descricao=" + descricao + "]";
