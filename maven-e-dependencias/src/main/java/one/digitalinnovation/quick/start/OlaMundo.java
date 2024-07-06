f808bffab1e45656c41ae4a38952c07883548bd3
Mattheus2403 <matheusmoraidois@gmail.com>
2024-07-05 22:29:52 -0300

    feat: add maven projects

---
+++ maven-e-dependencias/src/main/java/one/digitalinnovation/quick/start/OlaMundo.java
@@ -0,0 +1,10 @@
+package one.digitalinnovation.quick.start;
+
+//import one.digitalinnovation.component.Parser;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ projeto-teste-mvn-component/src/one/digitalinnovation/component/Parser.java
@@ -0,0 +1,8 @@
+package one.digitalinnovation.component;
+
+public class Parser {
+
+	public void message() {
+		System.out.println("Olá Mundo");
+	}
+}
